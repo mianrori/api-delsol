23463002988566e3f0c1f40ba053ccb0e11f3404
unknown <lutormig@gmail.com>
2023-10-17 18:08:09 -0300

    cambio de orden de extracto de cheque de ASC a DESC

--- src/database/get-extracto-chequedelsol.db.js
+++ src/database/get-extracto-chequedelsol.db.js
@@ -4,7 +4,8 @@ export const getExtractoChequeDelSolDb = (db, dni) => {
   return new Promise(async (resolve) => {
     try {
       const result = await db.execute(
-        `SELECT * FROM TABLE(fun_tab_cheque_sol_extracto(:dni))`,
+        `SELECT * FROM TABLE(fun_tab_cheque_sol_extracto(:dni)) a
+        ORDER BY a.fecha DESC`,
         [dni]
       );
       if (result.rows.length === 0) {
