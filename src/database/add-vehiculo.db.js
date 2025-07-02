80857cc1234f3d5b5438d77f7a0f05fd0f340b7e
unknown <lutormig@gmail.com>
2025-07-02 18:19:36 -0300

    agregar un vehículo

---
+++ src/database/add-vehiculo.db.js
@@ -0,0 +1,61 @@
+import oracledb from "oracledb";
+
+export const addVehiculoDb = (db, payload) => {
+  return new Promise(async (resolve, reject) => {
+    const { dni, matricula, marca, modelo, color } = payload;
+    try {
+      const result = await db.execute(
+        `BEGIN
+            pro_insertar_vehiculo_socio(:dni,:matricula,:marca,:modelo,:color);
+         END;`,
+        {
+          dni: {
+            dir: oracledb.BIND_IN,
+            val: dni,
+            type: oracledb.STRING,
+          },
+          matricula: {
+            dir: oracledb.BIND_IN,
+            val: matricula,
+            type: oracledb.STRING,
+          },
+          marca: {
+            dir: oracledb.BIND_IN,
+            val: marca,
+            type: oracledb.STRING,
+          },
+          modelo: {
+            dir: oracledb.BIND_IN,
+            val: modelo,
+            type: oracledb.STRING,
+          },
+          color: {
+            dir: oracledb.BIND_IN,
+            val: color,
+            type: oracledb.STRING,
+          },
+        }
+      );
+      resolve({
+        status: 200,
+        success: true,
+        message: "Vehículo creado exitosamente.",
+      });
+    } catch (error) {
+      let { message } = error;
+      message = message.split("\n")[0].split(":")[1].trim();
+      console.log(`Error en addVehiculoDb: ${error.message}`);
+      let status = 500;
+      if (error.message.includes("20002")) {
+        status = 409;
+      }
+      reject({
+        status,
+        success: false,
+        message,
+      });
+    }
+  });
+};
+
+export default { addVehiculoDb };
