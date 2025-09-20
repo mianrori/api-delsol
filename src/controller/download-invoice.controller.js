cc7e0ca936de731357350495034eea2a80a0ae87
unknown <lutormig@gmail.com>
2025-09-20 09:34:09 -0300

    path que apunta a kude del server de PROD

--- src/controller/download-invoice.controller.js
+++ src/controller/download-invoice.controller.js
@@ -1,8 +1,8 @@
 import path from "path";
 import fs from "fs";
-//const PDF_DIRECTORY = 'C:\\node\\sifen\\electronic-document\\kude\\invoices';
-const PDF_DIRECTORY =
-  "C:\\Users\\Miguel\\Documents\\Miguel\\Desarrollo\\Proyectos\\sifen\\electronic-document\\kude\\invoices";
+const PDF_DIRECTORY = "C:\\node\\sifen\\electronic-document\\kude\\invoices";
+/*const PDF_DIRECTORY =
+  "C:\\Users\\Miguel\\Documents\\Miguel\\Desarrollo\\Proyectos\\sifen\\electronic-document\\kude\\invoices";*/
 
 export const downloadInvoiceController = async (req, res) => {
   try {
@@ -12,24 +12,20 @@ export const downloadInvoiceController = async (req, res) => {
 
     // Verificar si el archivo existe
     if (!fs.existsSync(filePath)) {
-      return res
-        .status(404)
-        .json({
-          status: 404,
-          success: false,
-          message: "Archivo no encontrado",
-        });
+      return res.status(404).json({
+        status: 404,
+        success: false,
+        message: "Archivo no encontrado",
+      });
     }
 
     // Verificar si es un PDF
     if (path.extname(fileName).toLowerCase() !== ".pdf") {
-      return res
-        .status(400)
-        .json({
-          status: 400,
-          success: false,
-          message: "El archivo no es un PDF",
-        });
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: "El archivo no es un PDF",
+      });
     }
 
     // Configurar headers para la descarga
